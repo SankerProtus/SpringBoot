44b9e974ba0cdb05ed4da685701dc8983896e184
SankerProtus <sankerprotus04@gmail.com>
2025-06-02 21:15:32 +0000

    Initial commit

---
+++ src/main/java/com/codeQuest/Chatterly/DTOs/UpdateUserRequest.java
@@ -0,0 +1,21 @@
+package com.codeQuest.Chatterly.DTOs;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateUserRequest(
+        @NotBlank(message = "Username is required")
+        @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+        String username,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email should be valid")
+        String email,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password
+
+
+) {}
