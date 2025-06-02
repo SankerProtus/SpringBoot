d58caa09cac2114953d07b93c494ac672b13a49d
SankerProtus <sankerprotus04@gmail.com>
2025-06-02 22:24:00 +0000

    Add authentication controller and DTOs for login and registration

---
+++ src/main/java/com/codeQuest/Chatterly/Controllers/AuthControllers.java
@@ -0,0 +1,29 @@
+package com.codeQuest.Chatterly.Controllers;
+
+import com.codeQuest.Chatterly.DTOs.LoginDto;
+import com.codeQuest.Chatterly.DTOs.SignUpDto;
+import com.codeQuest.Chatterly.Services.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
+public class AuthControllers {
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginDto loginDto) {
+        return userService.login(loginDto.getUsername(), loginDto.getPassword());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpDto signUpDto) {
+        return userService.signUp(signUpDto);
+    }
+}
